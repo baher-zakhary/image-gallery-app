04b0cf75ec63d9ede2865188cb0882c2dd2171a4
baher-zakhary <baher.zakharyy@gmail.com>
2022-01-19 14:54:42 +0200

    - Display image title in popup

--- src/app/image-gallery/models/image.ts
+++ src/app/image-gallery/models/image.ts
@@ -36,7 +36,7 @@ export class Image {
     }
 
     getDescription() {
-        return `${this.title} - Description is the pattern of narrative development that aims to make vivid a place, object, character, or group.
+        return `Description is the pattern of narrative development that aims to make vivid a place, object, character, or group.
         Description is one of four rhetorical modes (also known as modes of discourse), along with exposition, argumentation, and narration.
         In practice it would be difficult to write literature that drew on just one of the four basic modes.`;
     }
